2e50beaf48f743b218f62850321b2a1bff00325a
Mikenziokah <mikenziokah@gmail.com>
2023-06-26 12:39:29 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,62 +1,55 @@
 #include "main.h"
-/**
-* _printf - displays formated outputs on the console
-* @format: formated characters
-* Return: Always 0
-*/
+
+/*
+ * _printf - prints formats to the console
+ * @format: digits and characters to be printed
+ * Return: 0
+ */
 int _printf(const char *format, ...)
 {
-    int x = 0;
-    va_list digits;
-    va_start(digits, format);
+	int x = 0;
+	va_list digits;
 
-    if (format == NULL)
-    return (-1);
-    for (x = 0; format[x]; x++)
-    {
-        if (format[x] != '%')
-        {
-            return (format[x]);
-        }
-        else
-        {
-            if (!format[x + 1] && format[x + 1] == NULL)
-            return (-1);
-            switch (format)
-            {
-                case 'c':
-                {
-                    int ch = va_arg(digits, char *);
-                    putchar(ch);
-                    x++;
-                    break;
-                }
-                case 's':
-                {
-                    char *str = va_arg(digits, char *);
-                    fputs(str, stdout);
-                    x += strlen(str);
-                    break;
-                }
-                case '%';
-                putchar('%');
-                x++;
-                break;
-                {
-                    else
-                    {
-                        putchar(format);
-                        x++;
-                    }
-                    format++;
-                }
-                va_end(digits);
-                return (x);
-                    }
-                }
-                }
-                }
-            }
-        }
-    }
+	if (format == NULL)
+		return (-1);
+	va_start(digits, format);
+	if (format == '%')
+	{
+		format++;
+		{
+			switch (format)
+			{
+				case 'c':
+					{
+						int ch = va_arg(digits, int);
+
+						putchar(ch);
+						x++;
+						break;
+					}
+				case 's':
+					{
+						char *str = va_arg(digits, char *);
+
+						fputs(str, stdout);
+						x += strlen(str);
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						x++;
+						break;
+					}
+					else
+					{
+						putchar(format);
+						x++;
+					}
+					format++;
+			}
+			va_end(digits);
+			return (0);
+		}
+	}
 }
